ed486efe028a605177e06cb4038f41cf259d4c59
ExtremBot1234 <44088452+ExtremBot1234@users.noreply.github.com>
2018-11-11 17:16:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -345,7 +345,7 @@ client.on('message', message => {
 
 
 
-const developers = ["id"]
+const developers = ["272808689688248320"]
 client.on('message', message => {
     var argresult = message.content.split(`472413769700474901`).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
